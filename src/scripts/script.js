e7342a92e7e45b6666773998bd03233991aa25cd
diego <diegonegocios21@outlook.com>
2023-06-06 17:35:58 -0300

    Completed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,24 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function openModal () {
+    const buttons = document.querySelector('.open__modal')
+    const modalContainer = document.querySelector('.modal')
+    const modalBackground = document.querySelector('.modal__background');
+
+    buttons.addEventListener('click', () => {
+      modalContainer.showModal()
+      modalBackground.style.display = 'block';
+    })
+}
+
+openModal()
+
+function closeModal() {
+    const closeModal = document.querySelector('.X')  
+    const modalContainer = document.querySelector('.modal')
+    const modalBackground = document.querySelector('.modal__background');
+    closeModal.addEventListener('click', () => {
+        modalContainer.close();
+        modalBackground.style.display = 'none';
+    })
+}
+
+closeModal()
